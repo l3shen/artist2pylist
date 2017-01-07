cfb0d706a2711ab3681f54a575e8dd41cfeba2fe
Kamil Krawczyk <kamil.krawczyk7343@gmail.com>
2017-01-06 19:12:31 -0500

    New base file. Scrapes newest setlist to a list containg spotify track IDs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 from bs4 import BeautifulSoup as parser
 import spotipy as spot
 import spotipy.util as spotutil
-import urllib2
+from urllib2 import urlopen
 import sys
 
 if len(sys.argv) == 3:
@@ -15,9 +15,6 @@ if len(sys.argv) == 3:
 else:
     print "artist2pylist usage:\npython main.py <Spotify username> <Artist> <Playlist Name>"
     sys.exit()
-
-# get desired artist from user via terminal; testing with generic artist
-# artist = 'Radiohead';
 
 # create playlist
 userToken = spotutil.prompt_for_user_token(username)
@@ -30,12 +27,34 @@ else:
     print("Error, cannot authenticate for user " + username + ", please double check username.")
 
 # url to Parse
+artist = 'Radiohead'
 # TODO make a class to pull desired artist URL
 url = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=' + artist + '&api_key=3a34aadae9198553ffd9123cb8c841e3'
 
 # acquire data
 # TODO add error handling for download, does not need to be async
-download = urllib2.urlopen(url)
-content = download.read()
 
-soup = parser(content, 'html.parser')
+# download link data from XML
+# create BS object
+soup = parser(urlopen(url), "lxml")
+
+topTracks = soup.find_all('track')
+playlistContent = []
+
+# iterate through all the top tracks and add to an array
+for tracks in topTracks:
+    item = tracks.contents[0]
+    playlistContent.append(item.text.encode("utf-8"))
+
+# sanity check
+print playlistContent
+
+# add tracks to playlist
+scope = 'playlist-modify-public'
+userToken = spotutil.prompt_for_user_token(username, scope)
+
+
+
+
+
+
